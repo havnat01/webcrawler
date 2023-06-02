181b1619dc2b0bd60cbe493db011c3f9330d4bdc
Minh Vu <vungocminh1692@gmail.com>
2023-06-02 19:31:12 +0700

    Init code

---
+++ src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -0,0 +1,55 @@
+package com.udacity.webcrawler.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.util.Objects;
+
+/**
+ * Utility class to write a {@link CrawlResult} to file.
+ */
+public final class CrawlResultWriter {
+  private final CrawlResult result;
+  static ObjectMapper objectMapper = new ObjectMapper();
+
+  /**
+   * Creates a new {@link CrawlResultWriter} that will write the given {@link CrawlResult}.
+   */
+  public CrawlResultWriter(CrawlResult result) {
+    this.result = Objects.requireNonNull(result);
+  }
+
+  /**
+   * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Path}.
+   *
+   * <p>If a file already exists at the path, the existing file should not be deleted; new data
+   * should be appended to it.
+   *
+   * @param path the file path where the crawl result data should be written.
+   */
+  public void write(Path path) {
+    try (FileWriter fileWriter = new FileWriter(Objects.requireNonNull(path).toFile(), true)){
+      write(fileWriter);
+    }
+    catch (IOException ex) {
+      ex.getLocalizedMessage();
+    }
+  }
+
+  /**
+   * Formats the {@link CrawlResult} as JSON and writes it to the given {@link Writer}.
+   *
+   * @param writer the destination where the crawl result data should be written.
+   */
+  public void write(Writer writer) {
+    objectMapper.disable(com.fasterxml.jackson.core.JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+    try {
+      objectMapper.writeValue(Objects.requireNonNull(writer), result);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -0,0 +1,58 @@
+package com.udacity.webcrawler.profiler;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+/**
+ * A method interceptor that checks whether {@link Method}s are annotated with the {@link Profiled}
+ * annotation. If they are, the method interceptor records how long the method invocation took.
+ */
+final class ProfilingMethodInterceptor implements InvocationHandler {
+
+  private final Clock clock;
+  private final Object delegate;
+  private final ProfilingState state;
+  private final ZonedDateTime startTime;
+
+  ProfilingMethodInterceptor(Clock clock, Object delegate,
+                             ProfilingState state, ZonedDateTime startTime) {
+
+    this.clock = Objects.requireNonNull(clock);
+    this.delegate = Objects.requireNonNull(delegate);
+    this.state = Objects.requireNonNull(state);
+    this.startTime = Objects.requireNonNull(startTime);
+  }
+
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    Object invokedObject;
+    Instant startTime = null;
+    boolean isProfiled = method.getAnnotation(Profiled.class) != null;
+    if (isProfiled) {
+      startTime = clock.instant();
+    }
+    try {
+      invokedObject = method.invoke(this.delegate, args);
+
+    } catch (InvocationTargetException ex) {
+      throw ex.getTargetException();
+    } catch (IllegalAccessException ex) {
+      // IllegalAccessException should be caught in a try-catch and
+      // wrapped in an unchecked exception such as RuntimeException
+      throw new RuntimeException(ex);
+
+    } finally {
+      if (isProfiled) {
+        Duration duration = Duration.between(startTime, clock.instant());
+        state.record(this.delegate.getClass(), method, duration);
+      }
+    }
+    return invokedObject;
+  }
+}
